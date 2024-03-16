82c58b0938ddbd5e5aba21e87cc840f5cda9dffe
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2024-03-16 01:01:11 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,185 @@
+<svelte:head>
+	<title>Cy Westbrook</title>
+	<meta
+		name="description"
+		content="ECE and CS student at Rutgers University (Engineering Honors Academy). Passionate for anything screens, circuits, software, stages, and sound."
+	/>
+	<meta property="og:title" content="Cy Westbrook" />
+	<meta property="og:site_name" content="Cy Westbrook" />
+	<meta
+		property="og:description"
+		content="ECE and CS student at Rutgers University (Engineering Honors Academy). Passionate for anything screens, circuits, software, stages, and sound."
+	/>
+	<meta property="og:image" content="https://cy2.me/c_general_192.png" />
+</svelte:head>
+
+<main>
+	<h1>👋 Cy Westbrook here.</h1>
+	<p>
+		ECE and CS student at Rutgers University (Engineering Honors Academy). <br
+		/> Passionate for anything screens, circuits, software, stages, and sound.
+	</p>
+	<div class="buttons">
+		<a
+			href="https://www.linkedin.com/in/cy-westbrook-925b25205/"
+			target="_blank"
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+				<path
+					d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+				/>
+			</svg>
+			LinkedIn
+		</a>
+		<a href="https://github.com/cyfinfaza" target="_blank">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+				<path
+					d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+				/>
+			</svg>
+			GitHub
+		</a>
+		<a href="https://www.instagram.com/westbrook.cy/" target="_blank">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				class="bi bi-instagram"
+				viewBox="0 0 16 16"
+			>
+				<path
+					d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"
+				/>
+			</svg>
+			Instagram
+		</a>
+		<a href="mailto:hi@cy2.me">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+				<path
+					d="M6.542 34.583Q4.708 34.583 3.417 33.25Q2.125 31.917 2.125 30.167V9.833Q2.125 8.042 3.417 6.708Q4.708 5.375 6.542 5.375H33.458Q35.292 5.375 36.604 6.708Q37.917 8.042 37.917 9.833V30.167Q37.917 31.917 36.604 33.25Q35.292 34.583 33.458 34.583ZM20 22.125Q20.292 22.125 20.562 22.042Q20.833 21.958 21.208 21.75L32.792 14.083Q33.042 13.917 33.25 13.583Q33.458 13.25 33.458 12.75Q33.458 11.958 32.667 11.417Q31.875 10.875 31.083 11.458L20 18.708L8.958 11.458Q8.167 10.958 7.354 11.396Q6.542 11.833 6.542 12.75Q6.542 13.208 6.75 13.542Q6.958 13.875 7.25 14.083L18.792 21.75Q19.167 21.958 19.438 22.042Q19.708 22.125 20 22.125Z"
+				/>
+			</svg>
+			Email
+		</a>
+	</div>
+	<h2>🌐 I've done a lot of web development...</h2>
+	<p>
+		For myself, my friends and teachers, clubs I'm a part of, nonprofits, and
+		even a national security project. Visit my web portfolio to check out all
+		the web sites and apps I've built from scratch.
+	</p>
+	<a href="https://cy2.me" target="_blank">Visit my web portfolio</a>
+	<h2>...and technical theater. 🎤</h2>
+	<p>
+		Yeah, I'm an engineer, but that doesn't mean I can't love theater. I've
+		worked on dozens of productions in school, community theater, and also some
+		professional settings since I first started in middle school. I work mostly
+		on live mixing audio, but have also spent a lot of time building custom
+		lighting electronics for sets. See the whole list of show's I've been a part
+		of and what I did for each one on my tech theater timeline.
+	</p>
+	<a href="https://cy2.me/technical-theater" target="_blank"
+		>See my tech theater timeline</a
+	>
+	<h2>Right now,</h2>
+	<p>
+		I'm focusing on building my electrical engineering skills and experience.
+		I'm part of several clubs at Rutgers, including <a
+			href="https://rusolarcar.com/"
+			target="_blank">Solar Car</a
+		>
+		(I'm the project manager) and the Rocket Propulsion Lab (telemetry). I also work
+		in the
+		<a href="https://sites.rutgers.edu/swarm-intelligence-lab/" target="_blank"
+			>Rutgers Swarm Intelligence Lab</a
+		> under Dr. Daniel Burbano (assistant professor ECE). I'm looking for internships
+		anywhere in the field of electronics and software. Download my resume below.
+	</p>
+	<a
+		href="https://docs.google.com/document/u/0/export?format=pdf&id=1kkvjPBAKhuvbgERyWO8s-R7lcR_AtRRbEaPAogvCALU"
+		>Download my resume</a
+	>
+	<div class="startDimmer"></div>
+	<div class="endDimmer"></div>
+</main>
+
+<style lang="scss">
+	main {
+		width: 100%;
+		max-width: 600px;
+		padding: 64px 24px;
+		> a {
+			border-left: 8px solid white;
+			font-weight: bold;
+			&::after {
+				content: "→";
+			}
+		}
+	}
+	.startDimmer,
+	.endDimmer {
+		height: 64px;
+		position: fixed;
+		left: 0;
+		width: 100%;
+	}
+	.startDimmer {
+		background: linear-gradient(180deg, #000f 0%, #0000 100%);
+		top: 0;
+	}
+	.endDimmer {
+		background: linear-gradient(0deg, #000f 0%, #0000 100%);
+		bottom: 0;
+	}
+	.buttons {
+		display: grid;
+		grid-template-columns: repeat(
+			auto-fill,
+			minmax(124px, 1fr) minmax(124px, 1fr)
+		);
+		gap: 8px;
+		a {
+			// flex-direction: column;
+			justify-content: flex-start;
+			padding: 8px;
+			margin: 0;
+			> * {
+				flex-shrink: 0;
+			}
+		}
+	}
+	a {
+		padding: 12px;
+		border-radius: 8px;
+		border: 1px solid #282828;
+		text-decoration: none;
+		display: flex;
+		margin: 8px 0;
+		align-items: center;
+		justify-content: space-between;
+		gap: 8px;
+		transition: 120ms;
+		flex: 1;
+		svg {
+			height: 1.5em;
+		}
+		&:hover {
+			background: #181818;
+			transform: translateY(-2px);
+		}
+	}
+	p a {
+		all: unset;
+		color: inherit;
+		text-decoration: underline;
+		cursor: pointer;
+	}
+	@media (max-width: 600px) {
+		main {
+			padding-top: 24px;
+		}
+		.startDimmer,
+		.endDimmer {
+			height: 24px;
+		}
+	}
+</style>
